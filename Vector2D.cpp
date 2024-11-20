fbcf3b531bec5ac934f2625283aa0c45459e3fee
The-Great-Nate <nathant1388@gmail.com>
2024-11-20 15:17:08 +0000

    files before I found out what was wanted

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -5,16 +5,18 @@ class Vector2D
 {
 public:
     Vector2D();                             // Default Constructor
-    Vector2D(double a, double b);             // Parameterised Constructor
-    double GetX() const;                     // Getter for x_
-    double GetY() const;                     // Getter for y_
+    Vector2D(double a, double b);           // Parameterised Constructor
+    double GetX() const;                    // Getter for x_
+    double GetY() const;                    // Getter for y_
     void SetStdNeighbour(Vector2D *buddy);  // Setter for nearest std neighbour
     void SetWrapNeighbour(Vector2D *buddy); // Setter for nearest wrap neighbour
+    double GetStdDistance() const;
+    double GetWrapDistance() const;
 
 private:
-    double x_; // X Co Ordinate
-    double y_; // Y Co Ordinate
-    Vector2D *std_neighbour_; // Pointer to the nearest standard neighbour - initially nothing hence nullptr in constructors
+    double x_;                 // X Co Ordinate
+    double y_;                 // Y Co Ordinate
+    Vector2D *std_neighbour_;  // Pointer to the nearest standard neighbour - initially nothing hence nullptr in constructors
     Vector2D *wrap_neighbour_; // Pointer to the nearest wrapped neighbour - initially nothing hence nullptr in constructors
 };
 
@@ -27,11 +29,10 @@ Vector2D::Vector2D() : x_(0.0),
 }
 
 // Parameterised Constructor
-Vector2D::Vector2D(double a, double b) :
-                                       x_(a),
-                                       y_(b),
-                                       std_neighbour_(nullptr),
-                                       wrap_neighbour_(nullptr)
+Vector2D::Vector2D(double a, double b) : x_(a),
+                                         y_(b),
+                                         std_neighbour_(nullptr),
+                                         wrap_neighbour_(nullptr)
 {
 }
 
@@ -57,4 +58,33 @@ void Vector2D::SetStdNeighbour(Vector2D *buddy)
 void Vector2D::SetWrapNeighbour(Vector2D *buddy)
 {
     wrap_neighbour_ = buddy;
+}
+
+double Vector2D::GetStdDistance() const
+{
+    double dx = std::abs(wrap_neighbour_->GetX() - x_);
+    double dy = std::abs(wrap_neighbour_->GetY() - y_);
+    return sqrt(dx * dx + dy * dy);
+}
+
+double Vector2D::GetWrapDistance() const
+{
+    if (std_neighbour_ == wrap_neighbour_)
+        return GetStdDistance();
+    else
+    {
+        double dx = std::abs(wrap_neighbour_->GetX() - x_);
+        double dy = std::abs(wrap_neighbour_->GetY() - y_);
+        if (dx > 0.5)
+        {
+            dx = 1.0f - dx;
+        }
+
+        if (dy > 0.5)
+        {
+            dy = 1.0f - dy;
+        }
+        // Calculate the final distance with pythagoras
+        return sqrt(dx * dx + dy * dy);
+    }   
 }
--- find_buddies.cpp
+++ find_buddies.cpp
@@ -2,6 +2,8 @@
 #include "Vector2D.cpp"
 #include "rng.cpp"
 
+double distance_sq, closest_sq, closest_wrap_sq, closest;
+
 // Generate random points on each slot in points vector
 void make_points(std::vector<Vector2D> &points, int size)
 {
@@ -11,11 +13,49 @@ void make_points(std::vector<Vector2D> &points, int size)
     }
 }
 
+void find_nearest_neighbour(std::vector<Vector2D> &points, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        distance_sq = 0;
+        closest_sq = std::numeric_limits<double>::infinity();
+        closest_wrap_sq = std::numeric_limits<double>::infinity();
+        for (int j = 0; j < size; ++j)
+        {
+            if (i != j)
+            {
+                // Find distance between 2 points
+                double dx = std::abs(points[j].GetX() - points[i].GetX());
+                double dy = std::abs(points[j].GetY() - points[i].GetY());
+                double wrap_dx, wrap_dy
+                // Checking if wrapping around is worth it.
+                // If x or y is over 0.5 apart, wrap it around
+                if (dx > 0.5)
+                    dx = 1.0f - dx;
+
+                if (dy > 0.5)
+                    dy = 1.0f - dy;
+
+                // Calculate the final distance with pythagoras
+                distance_sq = dx * dx + dy * dy;
+
+                if (distance_sq <= closest_sq)
+                {
+                    closest_sq = distance_sq;
+                    points[i].SetStdNeighbour(&points[j]);
+                    points[i].SetWrapNeighbour(&points[j]);
+                }
+            }
+        }
+    }
+}
+
 // main
 int main(int argc, char **argv)
 {
     // checks if user provided enough arguments in command line
-    if (argc < 2) {
+    if (argc < 2)
+    {
         std::cerr << "Please provide the number of points as a command line argument." << std::endl;
         return EXIT_FAILURE;
     }
@@ -25,7 +65,7 @@ int main(int argc, char **argv)
     std::vector<Vector2D> points;
     points.reserve(size); // Reserve space for vector in memory
 
-    init_rng(); // Initialise rng
+    init_rng();                // Initialise rng
     make_points(points, size); // Generate the points
 
     // for debugging :D
