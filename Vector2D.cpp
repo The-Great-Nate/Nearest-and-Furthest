7486ff1aa5e8d95d9476be5d968859b8db802213
The-Great-Nate <nathant1388@gmail.com>
2024-11-25 16:35:14 +0000

    Deleted redefinition of Class

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -2,31 +2,6 @@
 #include <cmath>
 #include "Vector2D.hpp"
 
-class Vector2D
-{
-public:
-    Vector2D();                             // Default Constructor
-    Vector2D(double a, double b);           // Parameterised Constructor
-    double getX() const;                    // Getter for x_
-    double getY() const;                    // Getter for y_
-    void setStdNeighbour(Vector2D *buddy);  // Setter for nearest neighbour in standard geometry
-    void setWrapNeighbour(Vector2D *buddy); // Setter for nearest neighbour in wraparound geometry
-    void setStdFaraway(Vector2D *buddy);    // Setter for furthest faraway in standard geometry
-    void setWrapFaraway(Vector2D *buddy);   // Setter for furthest faraway in wraparound geometry
-    double getStdNeighbourDistance() const;  // Returns distance between self and nearest neighbour in standard geometry
-    double getStdFarawayDistance() const;  // Returns distance between self and furthest faraway in standard geometry
-    double getWrapNeighbourDistance() const; // Returns distance between self and nearest neighbour in wraparound geometry
-    double getWrapFarawayDistance() const; // Returns distance between self and furthest faraway in wraparound geometry
-
-private:
-    double x_;                 // X Co Ordinate
-    double y_;                 // Y Co Ordinate
-    Vector2D *std_neighbour_;  // Pointer to the nearest standard neighbour - initially nothing hence nullptr in constructors
-    Vector2D *wrap_neighbour_; // Pointer to the nearest wrapped neighbour - initially nothing hence nullptr in constructors
-    Vector2D *std_faraway_;    // Pointer to the furthest standard faraway - initially nothing hence nullptr in constructors
-    Vector2D *wrap_faraway_;   // Pointer to the furthest wrapped faraway - initially nothing hence nullptr in constructors
-};
-
 // Default Constructor
 Vector2D::Vector2D() : x_(0.0),
                        y_(0.0),
